b31eab48f3415907726912c15069411dded3dee4
Diego <dms509@hotmail.com>
2023-01-31 00:41:05 +0100

    ⚡️ Improve some components

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,8 +1,8 @@
-export default function Stack({ StackIcon = null, iconColor = "white" }) {
+export default function Stack({ StackIcon = null}) {
   return (
-    <article class="m-3 rounded-xl bg-gradient-to-r from-amber-400 via-sky-400 to-blue-900 p-0.5 shadow-xl transition hover:animate-background hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] dark:shadow-gray-700/25">
-      <div class="rounded-[10px] bg-white p-4  dark:bg-gray-900 sm:p-6 ">
-        <StackIcon class={`h-24 w-24 text-${iconColor}`} />
+    <article className="m-3 rounded-xl w-[150px] bg-gradient-to-r from-amber-400 via-sky-400 to-blue-900 p-0.5 shadow-xl transition hover:animate-background hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] dark:shadow-gray-700/25">
+      <div className="rounded-[10px] bg-white p-4 dark:bg-gray-900">
+        <StackIcon className="w-full h-full" />
       </div>
     </article>
   );
--- src/components/StacksGroup.jsx
+++ src/components/StacksGroup.jsx
@@ -1,22 +1,19 @@
 import Stack from "./Stack";
-import { FaReact, FaGithub, FaSymfony, FaPhp } from "react-icons/fa";
+import { FaReact, FaGithub, FaSymfony, FaPhp, FaLaravel } from "react-icons/fa";
 import { SiJavascript, SiVisualstudiocode } from "react-icons/si";
 
 export default function MyStacks() {
   return (
-    <section class="bg-gray-900 text-white">
-      <div class="mx-auto max-w-3xl text-center">
-        <h1 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-          My stacks
-          <span class="sm:block"> Increase Conversion. </span>
-        </h1>
+    <section>
+      <div className="text-center pt-5">
+        <h1 className="text-4xl font-bold text-amber-400 p-3">Some of my technologies</h1>
       </div>
-      <div class="mx-auto max-w-screen-lg px-4 py-10 sm:flex lg:items-center align-middle justify-center">
+      <div className="mx-auto max-w-screen-lg px-4 py-10 flex items-center align-middle justify-center">
         <Stack StackIcon={FaPhp} />
         <Stack StackIcon={FaSymfony} />
         <Stack StackIcon={FaGithub} />
       </div>
-      <div class="mx-auto max-w-screen-lg px-4 py-10 sm:flex lg:items-center align-middle justify-center">
+      <div className="mx-auto max-w-screen-lg px-4 py-10 flex items-center align-middle justify-center">
         <Stack StackIcon={FaReact} />
         <Stack StackIcon={SiJavascript} />
         <Stack StackIcon={SiVisualstudiocode} />
