3d568181442b68664d1ca445c0a54b480afb0079
Diego <dms509@hotmail.com>
2023-01-31 23:44:13 +0100

    ✨ Added pokeball and description

--- src/components/StacksGroup.jsx
+++ src/components/StacksGroup.jsx
@@ -5,9 +5,6 @@ import { SiJavascript, SiVisualstudiocode } from "react-icons/si";
 export default function MyStacks() {
   return (
     <section>
-      <div className="text-center pt-5">
-        <h1 className="text-4xl font-bold text-amber-400 p-3">Some of my technologies</h1>
-      </div>
       <div className="mx-auto max-w-screen-lg px-4 py-10 flex items-center align-middle justify-center">
         <Stack StackIcon={FaPhp} />
         <Stack StackIcon={FaSymfony} />
