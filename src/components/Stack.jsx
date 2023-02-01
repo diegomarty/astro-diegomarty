86dcf78e13add05207491f428f8f01f655fdfc66
Diego <dms509@hotmail.com>
2023-02-02 00:32:45 +0100

    ⚡️ Improve stacks components

--- src/components/Stack.jsx
+++
@@ -1,9 +0,0 @@
-export default function Stack({ StackIcon = null}) {
-  return (
-    <article className="m-3 rounded-xl w-[150px] bg-gradient-to-r from-amber-400 via-sky-400 to-blue-900 p-0.5 shadow-xl transition hover:animate-background hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] dark:shadow-gray-700/25">
-      <div className="rounded-[10px] bg-white p-4 dark:bg-gray-900">
-        <StackIcon className="w-full h-full" />
-      </div>
-    </article>
-  );
-}
--- src/components/stacks/StacksGroup.jsx
+++ src/components/stacks/StacksGroup.jsx
@@ -4,17 +4,26 @@ import { SiJavascript, SiVisualstudiocode } from "react-icons/si";
 
 export default function MyStacks() {
   return (
-    <>
-      <div className="mx-auto max-w-screen-lg px-4 py-10 flex items-center align-middle justify-center">
+    <div className="grid grid-cols-2 md:grid-cols-3 ">
+      <div className="mx-auto">
         <Stack StackIcon={FaPhp} />
+      </div>
+      <div className="mx-auto">
         <Stack StackIcon={FaSymfony} />
+      </div>
+      <div className="mx-auto">
         <Stack StackIcon={FaGithub} />
       </div>
-      <div className="mx-auto max-w-screen-lg px-4 py-10 flex items-center align-middle justify-center">
+      
+      <div className="mx-auto">
         <Stack StackIcon={FaReact} />
+      </div>
+      <div className="mx-auto">
         <Stack StackIcon={SiJavascript} />
+      </div>
+      <div className="mx-auto">
         <Stack StackIcon={SiVisualstudiocode} />
       </div>
-    </>
+    </div>
   );
 }
