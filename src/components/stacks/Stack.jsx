6a9f5eb64e7e68bff39252cd8b79a2e29336e54d
Diego <dms509@hotmail.com>
2023-02-22 20:37:30 +0100

    📱 Fixed select like text svg on movile

--- src/components/stacks/Stack.jsx
+++ src/components/stacks/Stack.jsx
@@ -3,7 +3,7 @@ export default function Stack({ StackIcon = null , image = null}) {
     <article className="m-3 rounded-xl w-[125px] sm:w-[150px] bg-gradient-to-r from-amber-400 via-sky-400 to-blue-900 p-0.5 shadow-xl transition hover:animate-background hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] shadow-gray-700/25 hover:scale-110 hover:rotate-3">
       <div className="rounded-[10px] bg-gray-900 p-4">
         {image ? <img src={image} alt="stack" width="114px" height="114px" /> : null}
-        {StackIcon ? <StackIcon className="w-full h-full" /> : null}
+        {StackIcon ? <StackIcon className="w-full h-full select-none" /> : null}
       </div>
     </article>
   );
