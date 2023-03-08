36fd6fd39a99d5175a43673265b13243665cd2aa
Diego <dms509@hotmail.com>
2023-03-09 00:07:12 +0100

    🎨 Format tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+	plugins: [
+		require('tailwindcss'),
+		require('autoprefixer'),
+		require('@tailwindcss/line-clamp'),
+		require('daisyui'),
+	],
 	theme: {
 		extend: {
 			animation: {
@@ -14,10 +20,4 @@ module.exports = {
 			},
 		}
 	},
-	plugins: [
-		require('tailwindcss'),
-    	require('autoprefixer'),
-		require('@tailwindcss/line-clamp'),
-		require('daisyui')
-	],
 }
